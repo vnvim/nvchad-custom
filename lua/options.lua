03cc379c780e32d6c52e7099b3864bdc47657cd5
vivekweb2013 <vivekweb2013@gmail.com>
2025-02-07 23:20:23 +0530

    Do not use system clipboard by default

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,8 @@
 require "nvchad.options"
 
+-- do not create swap files
 vim.o.swapfile = false
+
+-- do not use system clipboard for yank and paste operations
+-- instead use `"+y` and `"+p` motions to copy to and paste from system clipboard.
+vim.o.clipboard = ""
